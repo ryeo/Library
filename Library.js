354d38a34c61363b41d619ff2df588a3c64f6c14
ryeo <robertyeo@hotmail.com>
2013-09-17 20:05:04 -0700

    more changes

--- Library.js
+++ Library.js
@@ -15,10 +15,33 @@ if (Meteor.isClient) {
     }
   });*/
 
-  Template.categories.lists = function () {
-      return lists.find({}, {sort: {Category: 1}});
-  };
+    Template.categories.lists = function () {
+        return lists.find({}, {sort: {Category: 1}});
+    };
 
+    // We are declaring the 'adding_category' flag
+    Session.set('adding_category', false);
+
+    // This returns true if adding_category has been assigned a value
+    //of true
+    Template.categories.new_cat = function () {
+        return Session.equals('adding_category',true);
+    };
+
+    Template.categories.events({
+        'click #btnNewCat': function (e, t) {
+            Session.set('adding_category', true);
+            Meteor.flush();
+            focusText(t.find("#add-category"));
+        }
+    });
+
+    /////Generic Helper Functions/////
+    //this function puts our cursor where it needs to be.
+    function focusText(i) {
+        i.focus();
+        i.select();
+    };
 
 }
 
